60f83fb59afcd9e7945482d16c28f3b0125e2b9f
Ragamaliga <mragamaliga@gmail.com>
2024-05-16 11:12:39 +0530

    person B commit

--- weather_app/weather.js
+++ weather_app/weather.js
@@ -2,7 +2,7 @@
 const apiKey = "20de24abfd7ee88f0385f2003c0576bc";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-         
+///person b merge request       
 async function checkWeather(cityName){
     try {
         const response = await fetch(`${apiUrl}${cityName}&appid=${apiKey}`);
